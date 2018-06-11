27cf79c794bcbcc43808e93079b39ddfb7c6c334
Zivlong <zhaojl1114@163.com>
2018-06-11 19:09:48 +0800

    the first commit

---
+++ e3-search-web/src/main/java/cn/e3mall/search/exception/GlobalExceptionResolver.java
@@ -0,0 +1,33 @@
+package cn.e3mall.search.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 全局异常处理器
+ * 
+ * @author zjl
+ */
+public class GlobalExceptionResolver implements HandlerExceptionResolver {
+
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionResolver.class);
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
+		// 打印控制台
+		ex.printStackTrace();
+		// 写日志
+		logger.debug("系统发生异常");
+		// 显示错误页面
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("error/exception");
+		return modelAndView;
+	}
+
+}
