8e6c8f8ea15a3c7148fc5f03b1a89d1f0419802e
Zivlong <zhaojl1114@163.com>
2018-06-11 19:07:55 +0800

    the first commit

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,55 @@
+package cn.e3mall.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.mapper.TbItemCatMapper;
+import cn.e3mall.pojo.TbItemCat;
+import cn.e3mall.pojo.TbItemCatExample;
+import cn.e3mall.pojo.TbItemCatExample.Criteria;
+import cn.e3mall.service.ItemCatService;
+
+/**
+ * 商品分类管理
+ * 
+ * @author zjl
+ */
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+
+	/**
+	 * 根据parentId查询子节点列表
+	 */
+	@Override
+	public List<EasyUITreeNode> getItemCatlist(long parentId) {
+		// 根据parentId查询子节点列表
+		TbItemCatExample example = new TbItemCatExample();
+		Criteria criteria = example.createCriteria();
+		// 设置查询条件
+		criteria.andParentIdEqualTo(parentId);
+		// 执行查询
+		List<TbItemCat> list = itemCatMapper.selectByExample(example);
+		// 创建返回结果List
+		List<EasyUITreeNode> resultList = new ArrayList<>();
+		// 把列表转换成EasyUITreeNode列表
+		for (TbItemCat tbItemCat : list) {
+			EasyUITreeNode node = new EasyUITreeNode();
+			// 设置属性
+			node.setId(tbItemCat.getId());
+			node.setText(tbItemCat.getName());
+			node.setState(tbItemCat.getIsParent() ? "closed" : "open");
+			// 添加到结果列表
+			resultList.add(node);
+		}
+		// 返回结果
+		return resultList;
+	}
+
+}
